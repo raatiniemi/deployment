0a6b2ca27dca470063d0c395f406daa84d36a971
Tobias Raatiniemi <me@thedeveloperblog.net>
2012-12-26 19:29:53 +0100

    Implemented support for additional rsync flags.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-# +------------------------------------------
+# +------------------------------------
+# | Exit codes
+# +--------------------------
+#	0 - Successful.
+#	1 - General failure.
+#	2 - File is missing.
+#	3 - Property not defined.
+
+# +------------------------------------
 # | Set up basic settings and file references.
-# +------------------
+# +--------------------------
 ## Assemble the environment variables.
 NAME=$1
 ENVIRONMENT="$NAME.properties"
 ENVIRONMENT_EXCLUDE="$NAME.exclude"
+
+## Rsync options.
+DEFAULT_RSYNC_OPTIONS="-av"
 
 # Check that the user has supplied an environment name.
 if [ -z $1 ]; then
@@ -33,7 +44,7 @@ if [ -a $ENVIRONMENT ]; then
 	if [ -z $DEPLOYMENT_SOURCE ]; then
 		echo
 		echo "'DEPLOYMENT_SOURCE' for deployment environment '$NAME' have not been defined."
-		exit $EXIT_PROPERTY_MISSING
+		exit 3
 	fi
 
 	# Check that the deployment target have been defined
@@ -49,7 +60,7 @@ if [ -a $ENVIRONMENT ]; then
 	if [ -z $DEPLOYMENT_HOST ]; then
 		echo
 		echo "'DEPLOYMENT_HOST' for deployment environment '$NAME' have not been defined."
-		exit $EXIT_PROPERTY_MISSING
+		exit 3
 	fi
 
 	# Check that the deployment user have been defined
@@ -57,7 +68,7 @@ if [ -a $ENVIRONMENT ]; then
 	if [ -z $DEPLOYMENT_USER ]; then
 		echo
 		echo "'DEPLOYMENT_USER' for deployment environment '$NAME' have not been defined."
-		exit $EXIT_PROPERTY_MISSING
+		exit 3
 	fi
 
 	# ----------------------------------------
@@ -68,6 +79,14 @@ if [ -a $ENVIRONMENT ]; then
 	# If the user do not have an accepted public key on the deployment
 	# server, the password field will be prompted.
 	# -----------------------------
+
+	# Check if the additional rsync options have been defined within
+	# environment properties. Use an empty string if it's not set.
+	if [ -z $DEPLOYMENT_RSYNC_OPTIONS ]; then
+		DEPLOYMENT_RSYNC_OPTIONS=""
+	fi
+	# Assemble the rsync option flags.
+	RSYNC_OPTIONS="$DEFAULT_RSYNC_OPTIONS $DEPLOYMENT_RSYNC_OPTIONS"
 
 	# Get the absolute path for the deployment source and assemble
 	# the full destination, with user, host, and target folder.
@@ -86,7 +105,7 @@ if [ -a $ENVIRONMENT ]; then
 	# Files that are not currently within the source folder will be deleted
 	# from the destination folder aswell. This will keep the remote destination
 	# clean from deprecated files, etc.
-	if rsync -av --delete --exclude-from $ENVIRONMENT_EXCLUDE $SOURCE $DESTINATION
+	if rsync $RSYNC_OPTIONS --exclude-from $ENVIRONMENT_EXCLUDE $SOURCE $DESTINATION
 	then
 		echo
 		echo "Deployment have been completed."
@@ -99,5 +118,5 @@ if [ -a $ENVIRONMENT ]; then
 else
 	# Handle error.
 	echo "No property file is available for the environment '$NAME'."
-	exit $EXIT_FILE_MISSING
+	exit 2
 fi
